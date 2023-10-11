34fcf4d69806066a3e63df37b90f1a288fe1183b
Thomas Marques <thomas.marques@edu.devinci.fr>
2023-10-11 08:53:03 +0200

    Init project

---
+++ pokemon.js
@@ -0,0 +1,17 @@
+class Pokemon {
+    constructor(name, attack, defense, hp, luck) {
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+    }
+
+    isLucky() {
+
+    }
+
+    attackPokemon() {
+        
+    }
+}
