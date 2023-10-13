faadc4ebcfae19b56ce49dbd1d4f83887b025767
Thomas Marques <thomas.marques@edu.devinci.fr>
2023-10-13 09:53:24 +0200

    🔨 ADD Bonus

--- valorant.js
+++ valorant.js
@@ -6,6 +6,8 @@ let manches = 0
 
 let spike = false
 
+console.log("Le match commence ! Le premier à 13 manches de gagnées remporte la victoire !\n.")
+
 while(nbManchesEquipe1 < 13 && nbManchesEquipe2 < 13) {
     let equipe1 = ["Omen", "Jett", "Phoenix", "Fade", "Chamber"]
     let equipe2 = ["Omen", "Jett", "Phoenix", "Fade", "Chamber"]
@@ -14,9 +16,11 @@ while(nbManchesEquipe1 < 13 && nbManchesEquipe2 < 13) {
     let player1 = equipe1[Math.floor(Math.random()*equipe1.length)]
     let player2 = equipe2[Math.floor(Math.random()*equipe2.length)]
 
-    if(rdmEquipe < 0.5) {
+    let rdmJettAttack = Math.random()
+
+    if((manches % 3 === 0 && player1 === "Jett" && rdmJettAttack < 0.8) || rdmEquipe < 0.5) {
         equipe2.splice(equipe2.indexOf(player2), 1)
-        console.log(player2 + ", des défenseurs, a été éliminé par " + player1)
+        console.log(player2 + ", des défenseurs, a été éliminé par " + player1 + ". Encore " + equipe2.length + " joueurs dans cette équipe.")
 
         let rdmSpike = Math.random()
         if(rdmSpike < 0.6) {
@@ -29,7 +33,7 @@ while(nbManchesEquipe1 < 13 && nbManchesEquipe2 < 13) {
     }
     else {
         equipe1.splice(equipe1.indexOf(player1), 1)
-        console.log(player1 + ", des attaquants, a été éliminé par " + player2)
+        console.log(player1 + ", des attaquants, a été éliminé par " + player2 + ". Encore " + equipe1.length + " joueurs dans cette équipe.")
 
         let rdmSpike = Math.random()
         if(rdmSpike < 0.4) {
@@ -41,37 +45,61 @@ while(nbManchesEquipe1 < 13 && nbManchesEquipe2 < 13) {
         }
     }
 
+    let probaOfVictory = 0.5
+
     while(equipe1.length != 0 && equipe2.length != 0) {
         let player1 = equipe1[Math.floor(Math.random()*equipe1.length)]
         let player2 = equipe2[Math.floor(Math.random()*equipe2.length)]
 
-        let probaOfVictory = Math.random()
+        let rdmProbaOfVictory = Math.random()
+
+        let rdmOmenSmoke = Math.random()
+        let rdmPhoenixFlash = Math.random()
+        let chanceOfBlind = Math.random()
+
+        if(spike) probaOfVictory = 0.7
+
+        if(player1 === "Phoenix" && rdmPhoenixFlash < 0.5) {
+            console.log("Phoenix lance une flash !")
+            if(chanceOfBlind < 0.8) {
+                probaOfVictory = 0.6
+                console.log("Sa flash aveugle les ennemis !")
+            }
+            else {
+                probaOfVictory = 0.3
+                console.log("Sa flash aveugle ses alliés !")
+            }
+        }
 
         switch(spike) {
             case true:
-                if(probaOfVictory < 0.7) {
+                if(rdmProbaOfVictory < probaOfVictory) {
                     equipe2.splice(equipe2.indexOf(player2), 1)
-                    console.log(player2 + ", des défenseurs, a été éliminé par " + player1)
+                    console.log(player2 + ", des défenseurs, a été éliminé par " + player1 + ". Encore " + equipe2.length + " joueurs dans cette équipe.")
                 }
                 else {
                     equipe1.splice(equipe1.indexOf(player1), 1)
-                    console.log(player1 + ", des attaquants, a été éliminé par " + player2)
+                    console.log(player1 + ", des attaquants, a été éliminé par " + player2 + ". Encore " + equipe1.length + " joueurs dans cette équipe.")
                 }
 
                 break
 
-            case false:                
-                if(probaOfVictory < 0.5) {
+            case false:
+                if(player1 === "Omen" && rdmOmenSmoke < 0.5) {
+                    probaOfVictory = 0.6
+                    console.log("Omen lance une smoke !")
+                }
+
+                if(rdmProbaOfVictory < probaOfVictory) {
                     equipe2.splice(equipe2.indexOf(player2), 1)
-                    console.log(player2 + ", des défenseurs, a été éliminé par " + player1)
+                    console.log(player2 + ", des défenseurs, a été éliminé par " + player1  + ". Encore " + equipe2.length + " joueurs dans cette équipe.")
                 }
                 else {
                     equipe1.splice(equipe1.indexOf(player1), 1)
-                    console.log(player1 + ", des attaquants, a été éliminé par " + player2)
+                    console.log(player1 + ", des attaquants, a été éliminé par " + player2 + ". Encore " + equipe1.length + " joueurs dans cette équipe.")
                 }
 
                 break
-
         }
     }
 
@@ -79,13 +107,13 @@ while(nbManchesEquipe1 < 13 && nbManchesEquipe2 < 13) {
 
     if(equipe1.length === 0) {
         nbManchesEquipe2++
-        console.log("Manche " + manches + " remportée par l'équipe des défenseurs ! Ils sont à " + nbManchesEquipe2 + " manches de gagnées.")
+        console.log(".\nManche " + manches + " remportée par l'équipe des défenseurs ! Ils sont à " + nbManchesEquipe2 + " manches de gagnées.\n.\n")
     }
     else {
         nbManchesEquipe1++
-        console.log("Manche " + manches + " remportée par l'équipe des attaquants ! Ils sont à " + nbManchesEquipe1 + " manches de gagnées.")
+        console.log(".\nManche " + manches + " remportée par l'équipe des attaquants ! Ils sont à " + nbManchesEquipe1 + " manches de gagnées.\n.\n")
     }
 }
 
-if(nbManchesEquipe1 === 13) console.log("Match terminé ! L'équipe des attaquants remporte la victoire !")
-else console.log("Match terminé ! L'équipe des défenseurs remporte la victoire !")
+if(nbManchesEquipe1 === 13) console.log(".\nMatch terminé ! L'équipe des attaquants remporte la victoire !")
+else console.log(".\nMatch terminé ! L'équipe des défenseurs remporte la victoire !")
